8fe7a3bcb0eb74e2b50023c870e12be8c52d5b9d
tarun-s-cloud <tarunsondhiya58@gmail.com>
2025-04-08 17:14:55 +0530

    almost complete

--- src/models/booking.model.js
+++ src/models/booking.model.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
 const BookingSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  fullname: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   phoneno:{type: Number,
     required: true
   },
   bookon: { type: Date, required: true },
-  // service: { type: String, required: true },
+  adult:{type:Number},
+  child:{type:Number,
+  },
   status: { type: String, enum: ['pending', 'confirmed', 'cancelled'], default: 'pending' }
 }, { timestamps: true });
 
