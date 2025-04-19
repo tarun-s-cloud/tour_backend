7fc93dd364f0d84ff4580f643f9c66b01c7d7f18
tarun-s-cloud <tarunsondhiya58@gmail.com>
2025-04-19 13:18:06 +0530

    change

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,18 +4,18 @@ import jwt from "jsonwebtoken";
 
 const register = async (req, res) => {
   try {
-    const { username, email, password,order} = req.body;
+    const { username, email, password} = req.body;
     if (!username || !email || !password) return res.status(400).json({ error: 'All fields are required' });
     
     const existingUser = await User.findOne({ email });
     if (existingUser) return res.status(400).json({ error: 'Email already registered' });
     
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword , order});
+    const newUser = new User({ username, email, password: hashedPassword});
     await newUser.save();
     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
     res.status(201).json({ message: 'User registered successfully', token ,
-       user: { id: newUser._id, username, email, password ,order}
+       user: { id: newUser._id, username, email, password}
      });
   } catch (error) {
     console.error("registration error: ",error)
